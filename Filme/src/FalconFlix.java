c5112189b5887a21d01af5dcbe7020a82b8ae598
Felipe Ribeiro <fehlepex@gmail.com>
2023-09-25 10:59:09 -0300

    Correções

--- Filme/src/FalconFlix.java
+++ Filme/src/FalconFlix.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class FalconFlix {
 
 public static void main(String[] args) {
+	String idade = null;
 	Scanner sc1 = new Scanner (System.in);
 	System.out.println("Qual o nome do Filme?");
 	String nome = sc1.nextLine();
@@ -13,10 +14,14 @@ public static void main(String[] args) {
 	System.out.println("Qual a faixa etária do Filme?");
 	byte faixaEt = sc1.nextByte();
 	
+	if (faixaEt == 0) {
+		idade = "Livre";
+	}
+	
 	System.out.println("Qual a duração do Filme?");
 	int durFilme = sc1.nextInt();
 	
-	System.out.println("Os dados do filme são:\n" + nome + "\n" + genero + "\n" + faixaEt + "\n" + durFilme + "minutos");
+	System.out.println("Os dados do filme são:\n" + nome + "\n" + genero + "\n" + idade + "\n" + durFilme + "minutos");
 	
 	sc1.close();
 }	
